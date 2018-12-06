a6b5bc094592b2811e403e742d0e2e5ac6882cfe
Juan I Diaz <jdi@idiaz.ca>
2018-12-06 10:52:11 -0500

    Initialize files... all empty but nice

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+// VARIABLES --------------------------------------------
+
+//      OBJECTS
+
+
+
+//      ARRAYS
+
+
+
+//      STRINGS/CHAR
+
+
+
+//      NUMBER/INTEGER
+
+
+
+//      BOOLEAN
+
+
+
+// ------------------------------------------------------------
+
+$(document).ready(function () {
+
+    console.log("Hello World!")
+
+});
